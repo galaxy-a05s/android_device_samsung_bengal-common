c80c1fdc3bb025a3c2bcf7d9cb818d4d5d173d20
Jamie Falconer <mrfluffyoven@gmail.com>
2025-03-09 09:30:09 +0000

    Add twrp ui alignment

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -51,6 +51,9 @@ BOARD_SUPPRESS_SECURE_ERASE := true
 TARGET_SCREEN_DENSITY := 393
 TARGET_SCREEN_HEIGHT := 1080
 TARGET_SCREEN_WIDTH := 2400
+#TW_CUSTOM_CPU_POS := 580
+TW_CUSTOM_CLOCK_POS := 50
+#TW_CUSTOM_BATTERY_POS := 800
 
 # Cmdline
 BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 firmware_class.path=/vendor/firmware_mnt/image printk.devkmsg=on bootconfig androidboot.hardware=qcom androidboot.memcg=1 androidboot.load_modules_parallel=true androidboot.usbcontroller=4e00000.dwc3 loop.max_part=7 androidboot.selinux=enforcing
