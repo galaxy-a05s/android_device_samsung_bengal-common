b411dee762c5782f57cacb7cecd5ffcabe9849eb
Jamie Falconer <mrfluffyoven@gmail.com>
2025-03-10 12:16:58 +0000

    Remove selinux flag

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -56,7 +56,7 @@ TW_CUSTOM_CLOCK_POS := 250
 #TW_CUSTOM_BATTERY_POS := 800
 
 # Cmdline
-BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 firmware_class.path=/vendor/firmware_mnt/image printk.devkmsg=on bootconfig androidboot.hardware=qcom androidboot.memcg=1 androidboot.load_modules_parallel=true androidboot.usbcontroller=4e00000.dwc3 loop.max_part=7 androidboot.selinux=enforcing
+BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 firmware_class.path=/vendor/firmware_mnt/image printk.devkmsg=on bootconfig androidboot.hardware=qcom androidboot.memcg=1 androidboot.load_modules_parallel=true androidboot.usbcontroller=4e00000.dwc3 loop.max_part=7
 
 # Kernel
 TARGET_KERNEL_ARCH := arm64
