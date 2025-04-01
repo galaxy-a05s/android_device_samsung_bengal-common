333f572dd50a81f64c1a456ba0614c493e337717
SavedByLight <mrfluffyoven@gmail.com>
2025-04-01 08:42:38 +0100

    Cleanup Common tree

--- device.mk
+++ device.mk
@@ -30,33 +30,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
-# Recovery modules/dependencies
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libion \
-    libxml2 \
-    libkeymint \
-    libkeymint_support \
-    libnetutils \
-    libdmabufheap \
-    android.hidl.memory@1.0 \
-    android.hidl.memory.token@1.0 \
-    android.hidl.allocator@1.0 \
-    vendor.display.config@1.0 \
-    vendor.display.config@2.0 \
-
-RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymint.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymint_support.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libnetutils.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libdmabufheap.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.memory@1.0.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.memory.token@1.0.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.allocator@1.0.so \
-    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
-    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so 
-
 # QCOM
 PRODUCT_PACKAGES += \
     qcom_decrypt \
