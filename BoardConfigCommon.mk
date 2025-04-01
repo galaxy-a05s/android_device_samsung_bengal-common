f425023f4b04a0123b3bd3c433ab23d6903066de
SavedByLight <mrfluffyoven@gmail.com>
2025-04-01 11:04:20 +0100

    update USB properties

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -158,7 +158,7 @@ TW_PREPARE_DATA_MEDIA_EARLY := true
 TW_INCLUDE_LIBRESETPROP := true
 TW_NO_LEGACY_PROPS := true
 TW_USE_NEW_MINADBD := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.0/file
 TW_CUSTOM_CPU_TEMP_PATH := "/sys/devices/virtual/thermal/thermal_zone50/temp"
 
 # Logging
