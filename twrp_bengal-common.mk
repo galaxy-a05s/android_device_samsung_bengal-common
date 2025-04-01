17563e5a224120e0a6bbe54ec1cf01b5a69d14aa
SavedByLight <mrfluffyoven@gmail.com>
2025-04-01 10:32:42 +0100

    add emulated storage

--- twrp_bengal-common.mk
+++ twrp_bengal-common.mk
@@ -12,6 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Inherit from a05s device
 $(call inherit-product, device/samsung/bengal-common/device.mk)
 
