2c46160e6658147c6c5206bee5cddbd5349d3b83
SavedByLight <mrfluffyoven@gmail.com>
2025-04-02 09:16:12 +0100

    mount dynamic partitions at boot, and add multidisabler

---
+++ recovery/root/system/bin/runatboot.sh
@@ -0,0 +1,9 @@
+#!/system/bin/sh
+mount -o ro /vendor
+mount -o ro /system
+mount -o ro /product
+mount -o ro /system_ext
+mount -o ro /vendor_dlkm
+mount -o ro /system_dlkm
+mount -o rw /metadata
+mount -o rw /vendor_boot
