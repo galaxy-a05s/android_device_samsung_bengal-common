382ac0e2028c97afa04142d0847ed2632cbe6048
SavedByLight <mrfluffyoven@gmail.com>
2025-04-02 08:49:59 +0100

    load modules

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -169,6 +169,10 @@ TW_INCLUDE_REPACKTOOLS := true
 #TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
 TW_CUSTOM_CPU_TEMP_PATH := "/sys/devices/virtual/thermal/thermal_zone26/temp"
 
+# Modules 
+TW_LOAD_VENDOR_BOOT_MODULES := true
+TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI := true
+
 # Logging
 TARGET_USES_LOGD := true
 TWRP_INCLUDE_LOGCAT := true
